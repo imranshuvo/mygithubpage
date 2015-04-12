f96a24fd6e8b7f9e424d7cbd6e89d5b9dcbbd4a0
Md. Imran Khan <imrankhanshuvo@gmail.com>
2015-04-13 03:49:08 +0600

    Added necessary things

--- js/app.js
+++ js/app.js
@@ -3,13 +3,34 @@ var shuvo = angular.module('shuvo',['ngRoute']);
 shuvo.config(['$routeProvider',function($routeProvider){
 	$routeProvider.
 		when('/',{
-			templateUrl: 'views/home.html',
-			controller: 'ListController'
+			templateUrl: 'views/home.html'
 		}).
-		when('/communcation',{
-			templateUrl: 'views/communcation.html'
+		when('/data-communication',{
+			templateUrl: 'views/communication.html'
 		}).
 		when('/difference',{
 			templateUrl: 'views/differences.html'
+		}).
+		when('/electromagnetic-fields-waves',{
+			templateUrl: 'views/electromagnetic.html'
+		}).
+		when('/numerical-analysis',{
+			templateUrl: 'views/numerical.html'
+		}).
+		when('/microprocessor-assembly',{
+			templateUrl: 'views/microprocessor.html'
+		}).
+		when('/electronic-material',{
+			templateUrl: 'views/material.html'
+		}).
+		when('/crystal',{
+			templateUrl: 'views/crystal.html'
+		}).
+		otherwise({
+			redirectTo: '/'
 		});
 }]);
+
+shuvo.controller('differenceCtrl',function($scope){
+
+});
